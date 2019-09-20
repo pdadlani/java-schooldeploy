72e9ea879f8ff490e434af6a4404538b163d6c7c
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-09-19 21:59:50 -0700

    added deployment.

---
+++ school/src/main/java/com/lambdaschool/school/repository/UseremailRepository.java
@@ -0,0 +1,11 @@
+package com.lambdaschool.school.repository;
+
+import com.lambdaschool.school.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UseremailRepository extends CrudRepository<Useremail, Long>
+{
+    List<Useremail> findAllByUser_Username(String name);
+}
---
+++ school/src/main/java/com/lambdaschool/school/services/UseremailService.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.school.services;
+
+import com.lambdaschool.school.models.Useremail;
+
+import java.util.List;
+
+public interface UseremailService
+{
+    List<Useremail> findAll();
+
+    Useremail findUseremailById(long id);
+
+    List<Useremail> findByUserName(String username);
+
+    void delete(long id, boolean isAdmin);
+
+    Useremail save(Useremail useremail, boolean isAdmin);
+}
